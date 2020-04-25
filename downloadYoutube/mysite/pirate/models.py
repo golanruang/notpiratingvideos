9d20598e0161b484b7f33b2fb8ae39b5f97ed1ad
golanruang <guang.rolan@gmail.com>
2020-04-24 23:24:58 -0700

    intial commit
    basic django stuff with pytube3 library
    basic template and models created

---
+++ downloadYoutube/mysite/pirate/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Video(models.Model):
+    link = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    Author = models.CharField(max_length=200)
+    description = models.CharField(max_length=4000)
+
+class Playlist(models.Model):
+    Videos = models.ForeignKey(Video, on_delete=models.CASCADE)
+    description = models.CharField(max_length=4000)
